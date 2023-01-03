1ce7c63d8956b6cb3928426428915e671f6315f8
EternalValkorion <kevinhi1234@gmail.com>
2023-01-03 10:54:46 +0000

    Ab1 Proto

---
+++ AB1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+    bcm2835_init();
+    bcm2835_gpio_fsel(GPIO18,<Funktion>);
+    for (int cycle = 0; i < 500; cycle++)
+    {
+        bcm2835_gpio_set(GPIO18);
+        bcm2835_delay(100);
+        bcm2835_gpio_clr(GPIO18);
+    }
+    
+    
+    
+    bcm2835_close();
+}
