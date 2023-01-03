666bc57c08c15bea3ff9d94988cc3ce0d4ad01c6
EternalValkorion <kevinhi1234@gmail.com>
2023-01-03 15:24:42 +0000

    AB1 Finished - it does blinki blink

--- AB1.c
+++ AB1.c
@@ -1,6 +1,10 @@
+// Starten mit folgendem gcc command: sudo gcc -Wall -o AB1 AB1.c -l bcm2835
+// ALS SUDO COMPILEN UND ALS SUDO AUSFUEHREN!!!
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <bcm2835.h>
+#include <unistd.h>
 
 int main()
 {
@@ -9,11 +13,7 @@ int main()
     bcm2835_gpio_fsel(25, 0x01);
     bcm2835_gpio_fsel(13, 0x01);
 
-    bcm2835_gpio_clr(18);
-    bcm2835_gpio_clr(25);
-    bcm2835_gpio_clr(13);
-
-    for (int cycle = 0; cycle < 10; cycle++)
+    do
     {
         bcm2835_gpio_set(18);
         bcm2835_gpio_set(25);
@@ -24,13 +24,29 @@ int main()
         bcm2835_gpio_clr(18);
         bcm2835_gpio_clr(25);
         bcm2835_gpio_clr(13);
-    }
+
+        bcm2835_delay(1000);
+
+    }while(1);
+
+
+    /*for (int cycle = 0; cycle < 10; cycle++)
+    {
+        bcm2835_gpio_set(18);
+        bcm2835_gpio_set(25);
+        bcm2835_gpio_set(13);
+
+        bcm2835_delay(1000);
+
+        bcm2835_gpio_clr(18);
+        bcm2835_gpio_clr(25);
+        bcm2835_gpio_clr(13);
+    }*/
 
     bcm2835_close();
 
     return 0;
 }
-
 
 
 
