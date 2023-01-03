e4936a7b4fb186b28185a402295c358cf23107c1
EternalValkorion <kevinhi1234@gmail.com>
2023-01-03 10:55:27 +0000

    Update AB1.c

--- AB1.c
+++ AB1.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+
 void main()
 {
     bcm2835_init();
     bcm2835_gpio_fsel(GPIO18,<Funktion>);
-    for (int cycle = 0; i < 500; cycle++)
+    for (int cycle = 0; i < 20; cycle++)
     {
         bcm2835_gpio_set(GPIO18);
         bcm2835_delay(100);
