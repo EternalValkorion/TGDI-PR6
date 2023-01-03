e0cf2277827dd9803d416e7c7deddda20efb6e65
EternalValkorion <kevinhi1234@gmail.com>
2023-01-03 15:05:16 +0000

    An - Aus noch kein Blink Blink

--- AB1.c
+++ AB1.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <bcm2835.h>
 
 int main()
 {
     bcm2835_init();
-   // bcm2835_gpio_fsel(GPIO18,<Funktion>);
-    for (int cycle = 0; cycle < 20; cycle++)
+    bcm2835_gpio_fsel(18, 0x01);
+    bcm2835_gpio_fsel(25, 0x01);
+    bcm2835_gpio_fsel(13, 0x01);
+
+    bcm2835_gpio_clr(18);
+    bcm2835_gpio_clr(25);
+    bcm2835_gpio_clr(13);
+
+    for (int cycle = 0; cycle < 10; cycle++)
     {
         bcm2835_gpio_set(18);
         bcm2835_gpio_set(25);
         bcm2835_gpio_set(13);
-        bcm2835_delay(100);
+
+        bcm2835_delay(1000);
+
         bcm2835_gpio_clr(18);
+        bcm2835_gpio_clr(25);
+        bcm2835_gpio_clr(13);
     }
+
     bcm2835_close();
+
     return 0;
 }
-
-
-
-
-
-
 
 
 
