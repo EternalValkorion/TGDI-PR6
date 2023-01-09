478bd4a237c16e39e43dd271bf9fb8afda6743bb
EternalValkorion <69819009+EternalValkorion@users.noreply.github.com>
2023-01-09 18:05:13 +0100

    AB2 Fixed & Optimized

--- AB2.c
+++ AB2.c
@@ -7,6 +7,7 @@ int main()
 {
 
 char buffer[12];
+int count = 1;
 
 // Initialisation and configuration of used pins.
 bcm2835_init();
@@ -17,33 +18,38 @@ bcm2835_i2c_setClockDivider(2500);
 
 // Address of corresponding Temp sensor.
 bcm2835_i2c_setSlaveAddress(0x48);
-for(int cycle = 0; cycle < 11; cycle++)
-{
-        bcm2835_i2c_read(buffer, 2);
-}
 
-        //Shift Values to MSB. Value will be transformed without LSB part to not show float values. (deprecated comment)
-        //int16_t i16 = data_read[0] << 8 | data_read[1];
-        //float temp = i16 / 256.0;
+//Selection of gpio pins for activation of LED on successfull Print of Temperatures
+bcm2835_gpio_fsel(18, 0x01);
+bcm2835_gpio_fsel(25, 0x01);
+bcm2835_gpio_fsel(13, 0x01);
 
-        // Printing Results
-        for(int i = 0; i < 12; i++)
+        //Shift Values to MSB and Printing Results
+        for(int i = 0; i < 100; i++)
         {
-                int readshift = buffer[0] << 8 | buffer[1];
-                float temp = readshift / 256;
-                printf("Temperature = %.3f\r\n",temp);
+                for(int cycle = 0; cycle < 11; cycle++)
+                {
+                        bcm2835_i2c_read(buffer, 2);
+                }
 
+                float readshift = buffer[0] << 7 | buffer[1];
+                float temp = readshift / 256;
+                printf("%d.Temperature = %.3f Grad Celsius\r\n", count, temp);
+
+                count++;
+
+                // Commands for LEDS.
+                bcm2835_gpio_set(18);
+                bcm2835_gpio_set(25);
+                bcm2835_gpio_set(13);
+
+                bcm2835_delay(1000);
+
+                bcm2835_gpio_clr(18);
+                bcm2835_gpio_clr(25);
+                bcm2835_gpio_clr(13);
         }
 
-        //Selection of gpio pins for activation of LED on successfull Print of Temperatures
-        bcm2835_gpio_fsel(18, 0x01);
-        bcm2835_gpio_fsel(25, 0x01);
-        bcm2835_gpio_fsel(13, 0x01);
-
-        // Activationcommands for LEDS.
-        bcm2835_gpio_set(18);
-        bcm2835_gpio_set(25);
-        bcm2835_gpio_set(13);
 
 // Revertion of pin configuration to default.
 bcm2835_i2c_end();
@@ -52,3 +58,4 @@ bcm2835_close();
 
 return 0;
 }
+
