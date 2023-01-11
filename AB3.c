3a953cca7e370095e72ba8fb3d4c40ff2534c8e8
EternalValkorion <69819009+EternalValkorion@users.noreply.github.com>
2023-01-11 15:45:04 +0100

    FINAL AB3

--- AB3.c
+++ AB3.c
@@ -2,22 +2,35 @@
 #include <bcm2835.h>                                        // Header fuer Broadcom
 
 #define PIN                             RPI_BPLUS_GPIO_J8_12                                                    // PWM-Pin
-#define PWM_CHANNEL             0                                                                                               // PWM-Channel
+#define PWM_CHANNEL             0                                                                                               // PWM-C>
 #define PWM_RANGE               1024                                                                                    // PWM-Range
-#define SPEED                   1                                                                                               // Geschwindigkeit
+#define SPEED                   1                                                                                               // Gesch>
 
 int main(void){
         if(bcm2835_init()==0) return 1;                                                                         // Library initalisieren
 
         do{
         bcm2835_gpio_fsel(18, 0x02);
-        bcm2835_pwm_set_clock(1);
+        bcm2835_pwm_set_clock(5);
         bcm2835_pwm_set_mode(0,1,1);
         bcm2835_pwm_set_range(0, 1024);
-        bcm2835_pwm_set_data(0, 90);
-        bcm2835_delay(2000);
+
+        for(int intensity = 100; intensity < 500; intensity+=100)
+        {
+        bcm2835_pwm_set_data(0, intensity);
+        bcm2835_delay(100);
+        }
+
+        for(int intensity = 500; intensity > 100; intensity-=100)
+        {
+        bcm2835_pwm_set_data(0, intensity);
+        bcm2835_delay(100);
+        }
+
+
+        //bcm2835_delay(2000);
         }while(1);
-        // PWM Funktionalität an Pin freischalten
+// PWM Funktionalität an Pin freischalten
 
         // PWM Frequenz einstellen
 
@@ -32,11 +45,6 @@ int main(void){
         if(bcm2835_close()==0) return 1;                                                                        // Library schließen
         return 0;                                                               // Programmende
 }
-
-
-
-
-
 
 
 
