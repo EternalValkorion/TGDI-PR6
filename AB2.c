76eae19b56dd7a138ec2f896144c31bbe77460c4
EternalValkorion <69819009+EternalValkorion@users.noreply.github.com>
2023-01-11 16:54:25 +0100

    Finished

--- AB2.c
+++ AB2.c
@@ -6,9 +6,9 @@
 int main()
 {
 
-char buffer[12];
+char buffer[2];
 int count = 1;
-float minustemp = 250;
+
 // Initialisation and configuration of used pins.
 bcm2835_init();
 bcm2835_i2c_begin();
@@ -25,28 +25,18 @@ bcm2835_gpio_fsel(18, 0x01);
         //Shift Values to MSB and Printing Results
         for(int i = 0; i < 200; i++)
         {
-                for(int cycle = 0; cycle < 11; cycle++)
-                {
-                        bcm2835_i2c_read(buffer, 2);
-                }
+               bcm2835_i2c_read(buffer, 2);
 
+                int16_t readshift = (buffer[0] << 8) + buffer[1];
 
-                float readshift = buffer[0] << 8 | buffer[1];
-                float temp = readshift / 256;
+                readshift = readshift >> 5;
 
-                if(temp >= 0 && temp <= 150)
-                {
-                        printf("%d.Temperature = %.3f Grad Celsius\r\n", count, temp);
-                        count++;
-                }else
-                {
-                        minustemp = 256-temp;
-                        printf("%d.Temperature = -%.3f Grad Celsius\r\n", count, minustemp);
-                        count++;
-                }
+                float temp = (float)readshift/8;
+                printf("%d. Temperature = %.3f Grad Celsius\r\n", count, temp);
+                count++;
 
                 // Commands for LEDS.
-                if(temp <= 4 || minustemp <= 150)
+                if(temp <= 4)
                 {
                         printf("FROSTWARNUNG es ist unter +4 GRAD CELSIUS\r\n");
                         bcm2835_gpio_set(18);
@@ -54,7 +44,6 @@ bcm2835_gpio_fsel(18, 0x01);
                         bcm2835_delay(1000);
 
                         bcm2835_gpio_clr(18);
-                        minustemp = 151;
 
                 }
 
@@ -69,5 +58,3 @@ bcm2835_close();
 
 return 0;
 }
-
-
